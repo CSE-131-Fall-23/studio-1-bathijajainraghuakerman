f18702198178e2c7ed3596d77f435a2d3dbc3b44
apple <apple@localhost>
2023-09-06 13:54:16 -0500

    Average

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a number ");
+		int n1 = in.nextInt();
+		System.out.print("Enter another number ");
+		int n2 = in.nextInt();
+		System.out.print("the average of the two numbers are " + ((n1+n2)/2.0));
+		
+
+	}
+
+}
